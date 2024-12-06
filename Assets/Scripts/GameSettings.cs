cabb4cec5cecb066db8f44775750a07a98e34e24
snduhiu43 <shirleen.nduhiu43@gmail.com>
2024-12-07 01:27:59 +0300

    Initial commit

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    private int settings;
+    private const int settingsNumber = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
